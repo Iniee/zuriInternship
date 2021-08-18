abf6ee3a75d0a12cb3535fb6f5320432fa8c5b6b
Inioluwa <Inioluwa Alake>
2021-08-18 13:04:12 +0100

    message

--- index.php
+++ index.php
@@ -8,8 +8,16 @@
      <body>
      
        <?php
-          $connect = mysqli_connect('localhost', 'root', '', 'login')
-           or exit("Error from Database Connection");
+       $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+       $cleardb_server = $cleardb_url["host"];
+       $cleardb_username = $cleardb_url["user"];
+       $cleardb_password = $cleardb_url["pass"];
+       $cleardb_db = substr($cleardb_url["path"],1);
+       $active_group = 'default';
+       $query_builder = TRUE;
+       
+      $connect = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db)
+      or exit("Error from Database Connection");
          $regError= array();
          $regSuccess = array();
             
